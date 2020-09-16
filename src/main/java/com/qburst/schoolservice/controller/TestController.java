a5d05c9cc854bc3e012631c0ea287d8fa04291b1
hmdonly <hamdhanthayyil@gmail.com>
2020-09-16 22:42:33 +0530

    test controller

---
+++ src/main/java/com/qburst/schoolservice/controller/TestController.java
@@ -0,0 +1,41 @@
+package com.qburst.schoolservice.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+
+public class TestController {
+    {
+
+        //school controller
+        @Autowired
+        RestTemplate restTemplate;
+
+        //hysterix added comment
+        @HystrixCommand(fallbackMethod = "defaultStudentDetails")
+        @GetMapping(value = "/getSchoolDetails/{schoolname}")
+        public String getStudents(@PathVariable String schoolname)
+        {
+
+
+            String response = restTemplate.exchange("http://student-service/getStudentDetailsForSchool/{schoolname}",
+                    HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, schoolname).getBody();
+
+
+
+            return "School Name is  " + schoolname + " \n Student Details " + response;
+        }
+
+        private String defaultStudentDetails(String name) {
+        return "Hello User!"+name;
+    }
+
+    private dummyPrivateMethod(){
+
+    }
+    }
+}
